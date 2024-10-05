4863590731675253ec552a2746117eb152c76a8f
backpack <lihungkwan72@gmail.com>
2024-10-05 23:46:59 +1000

    -update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const serverless = require('serverless-http');
+const axios = require('axios');
 require('dotenv').config()
 
 const app = express();
@@ -11,6 +11,26 @@ app.use(cors());
 app.use(express.static('static'));
 
 
+app.get('/.netlify/functions/event', async function(req, res) {
+    const pageId = process.env.FACEBOOK_PAGE_ID; // page ID
+    const accessToken = process.env.FACEBOOK_ACCESS_TOKEN; //access token
+
+    try {
+        const response = await axios.get(`https://graph.facebook.com/v17.0/${pageId}/events?time_filter=upcoming&access_token=${accessToken}`);
+
+        const events = response.data.data.map(event => ({
+            name: event.name,
+            startTime: event.start_time,
+            location: event.place ? event.place.name : null,
+            url: `https://www.facebook.com/events/${event.id}`,
+        }));
+
+        res.status(200).json(events);
+
+    } catch (error) {
+        console.error('There was an error:', error.message);
+        res.status(500).json({ error: 'Unable to fetch events' });
+    }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-module.exports.handler = serverless(app);
